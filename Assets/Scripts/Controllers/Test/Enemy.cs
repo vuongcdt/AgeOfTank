841b837d9d02b293a322a947543c5a2705a0209d
huynhthuan <huynhthuanvugia@gmail.com>
2024-08-03 15:40:13 +0700

    add test

---
+++ Assets/Scripts/Controllers/Test/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+namespace Controllers.Test
+{
+    public class Enemy :MonoBehaviour
+    {
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if (other.collider.CompareTag(tag))
+            {
+                return;
+            }
+            GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+        }
+
+        // private void OnCollisionStay2D(Collision2D other)
+        // {
+        //     GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+        // }
+        
+        private void OnCollisionExit2D(Collision2D other)
+        {
+            if (other.collider.CompareTag(tag))
+            {
+                return;
+            }
+            GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+        }
+    }
+}
---
+++ Assets/Scripts/Controllers/Test/Player.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+namespace Controllers.Test
+{
+    public class Player : MonoBehaviour
+    {
+        [SerializeField] private Vector3 target = Vector3.right * 3;
+        [SerializeField] private int mass = 1;
+
+        private Vector3 _dir;
+        private Rigidbody2D _rg;
+
+        private void Start()
+        {
+            _rg = GetComponent<Rigidbody2D>();
+            _rg.mass = 1;
+        }
+
+        private void FixedUpdate()
+        {
+            _dir = (target - transform.position).normalized / 6;
+            _rg.AddForce(_dir);
+        }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if (!other.collider.CompareTag(tag))
+            {
+                _rg.mass = mass;
+                // _rg.velocity = Vector3.zero;
+            }
+            // _rg.mass = 2;
+        }
+
+
+        // private void OnCollisionExit2D(Collision2D other)
+        // {
+        //     _dir = (target - transform.position).normalized / 6;
+        //     _rg.velocity = _dir;
+        // }
+    }
+}
